74d516e553fd5ab6b0f2198d6cc998130b7cbd62
Linus Jansson <contact@limpan.dev>
2024-06-27 21:28:58 +0200

    enough for today

--- app/Word.tsx
+++ app/Word.tsx
@@ -10,25 +10,31 @@ export const SpeedTyper = ({ sentence }: {sentence:string}) => {
     const [correct, setCorrect] = useState<number[]>([]);
 
     const [_sentence, _setSentence] = useState(sentence);
+    const [value, setValue] = useState(sentence);
 
     const inputRef = useRef<HTMLInputElement>(null);
 
-    
+    const keysToReactTo = "abcdefghijklmnopqrstuvwxyz ',.?!ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
     const handleKeyPress = (event: any) => {
         event.preventDefault();
-        
+
         // if backspace is pressed undo
         if (event.keyCode === 8 || event.keyCode === 46) {
             console.log("backspace")
-            if (currentPosition > 0) {
+            if (currentPosition >= 0) {
                 setCurrentPosistion(prev => prev-1);
                 // remove from arrays
-                setCorrect((prev) => prev.filter((index) => index !== currentPosition - 1));
-                setWrong((prev) => prev.filter((index) => index !== currentPosition - 1));
+                setCorrect(prev => prev.filter((index) => index !== currentPosition - 1));
+                setWrong(prev => prev.filter((index) => index !== currentPosition - 1));
                 return;
             }
         }
-        
+
+        if (!keysToReactTo.includes(event.key)) {
+            return;
+        }
+
         const currentLength = event.target.value.length;
         const currentLetter = sentence[currentPosition];
         console.log(event.target.value);
@@ -37,9 +43,9 @@ export const SpeedTyper = ({ sentence }: {sentence:string}) => {
 
         // if correct letter
         if (currentLetter === event.target.value[currentLength - 1]) {
-            setCorrect((prev) => [...prev, currentPosition]);
+            setCorrect(prev => [...prev, currentPosition]);
         } else {
-            setWrong((prev) => [...prev, currentPosition]);
+            setWrong(prev => [...prev, currentPosition]);
         }
 
         setCurrentPosistion(prev => prev+1);
@@ -60,27 +66,27 @@ export const SpeedTyper = ({ sentence }: {sentence:string}) => {
     return (
         <div className="grid w-screen h-screen place-items-center" tabIndex={-1} onMouseUp={selectInput}>
             <div className="flex flex-row">
-                <p className="p-2">{correct.length} / {_sentence.length}</p>
+                <p className="p-2">{correct.length} / {sentence.length}</p>
                 <button className="p-2 bg-red-400 rounded-3xl font-bold" onClick={handleReset}>reset</button>
             </div>
             <div className="flex flex-wrap w-1/2">
-                {/* // bg-slate-600 bg-red-500 bg-slate-300 text-slate-600 text-red-500 text-slate-300 */}
-                {_sentence.split('').map((letter, index) => {
+                {/* // bg-green-600 bg-red-500 bg-slate-300 text-green-600 text-red-500 text-slate-300 */}
+                {sentence.split('').map((letter, index) => {
                     const isCorrect = correct.includes(index);
                     const isWrong = wrong.includes(index);
-                    const bg = isCorrect ? 'slate-600' : isWrong ? 'red-500' : "";
-                    const color = isCorrect ? 'slate-600' : isWrong ? 'red-500' : 'slate-300';
-
+                    const bg = isCorrect ? 'green-600' : isWrong ? 'red-500' : "";
+                    const color = isCorrect ? 'green-600' : isWrong ? 'red-500' : 'slate-300';
+                    const underline = currentPosition === index ? 'underline ' : '';
                     // logic for handling spaces correct and wrong
                     if (letter === ' ') {
-                        return <span key={index} className={"text-3xl " + bg}>&nbsp;</span>
+                        return <span key={index} className={"text-3xl " + bg + " " + underline}>&nbsp;</span>
                     }
 
                     // logic for hadnling correct and wrong letters
-                    return <span key={index} className={"text-3xl " + "text-" +color}>{letter}</span>
+                    return <span key={index} className={"text-3xl " + "text-" + color + " " + underline}>{letter}</span>
                 })}
             </div>
-            <input className="absolute top-0 left-0 opacity-0" type="text" ref={inputRef} onChange={handleKeyPress} />
+            <input className="absolute top-0 left-0 opacity-0" type="text" ref={inputRef} value={value} onKeyDown={handleKeyPress} />
         </div>
     )
 
